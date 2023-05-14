fd20e7cfff1bb6e3155d6f235582a37ed8082659
zlordelo <joao.system1@gmail.com>
2023-05-14 01:19:16 -0300

    finish content

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-title.component.css']
 })
 export class MenuTitleComponent {
-
+  @Input()
+  title: string = ""
 }
